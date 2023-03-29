dfbeddc4ee959f4ad52f6dd4aa446b977812e105
aminahagi <amina.hagi@student.moringaschool.com>
2023-03-29 13:04:44 +0300

    changes made in file

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,9 +3,9 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as BiIcons from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import { SidebarData } from './sidebarData';
+import { SidebarData } from './SidebarData'
 import { IconContext } from 'react-icons';
-import './navbar.css';
+import './Navbar.css';
 
 function Navbar(props) {
     const { handleLogout } = props;
@@ -15,45 +15,45 @@ function Navbar(props) {
     const showSidebar = () => setSidebar(!sidebar)
 
     return (
-        <>
-        <IconContext.Provider value={{ color: '#fff' }}>
-            <div className="navbar">
-                <Link to="#" className='menu-bars'>
-                    <FaIcons.FaBars onClick={showSidebar}/> 
-                </Link>
-                <Link to="/login" className='logout' title="Logout">
-                    <BiIcons.BiExit 
-                        onClick={handleLogout}
-                    /> 
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active ' : 'nav-menu' }>
-                <ul className='nav-menu-items' onClick={showSidebar}>
-                    <li className="navbar-toggle">
-                        <Link to="#" className='menu-bars'>
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
-                    <div className="loggedInAs">
-                        <p>Logged in as: </p>
-                        <h3>username</h3>
-                    </div>
-                    {/* maps all sidebar links */}
-                    {SidebarData.map((item, index) => {
-                        return (
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link> 
-                            </li>
-                        )
-                    })}                
-                </ul>
-            </nav>
+        <div className="navbar-wrapper">
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <div className="navbar">
+                    <Link to="#" className='menu-bars'>
+                        <FaIcons.FaBars onClick={showSidebar}/> 
+                    </Link>
+                    <Link to="#" className='logout' title="Logout">
+                        <BiIcons.BiExit 
+                            onClick={handleLogout}
+                        /> 
+                    </Link>
+                </div>
+                <nav className={sidebar ? 'nav-menu active ' : 'nav-menu' }>
+                    <ul className='nav-menu-items' onClick={showSidebar}>
+                        <li className="navbar-toggle">
+                            <Link to="#" className='menu-bars'>
+                                <AiIcons.AiOutlineClose />
+                            </Link>
+                        </li>
+                        <div className="loggedInAs">
+                            <p>Logged in as: </p>
+                            <h3>username</h3>
+                        </div>
+                        {/* maps all sidebar links */}
+                        {SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link> 
+                                </li>
+                            )
+                        })}                
+                    </ul>
+                </nav>
             </IconContext.Provider>  
-        </>
+        </div>
     )
 }
 
-export default Navbar
+export default Navbar;
